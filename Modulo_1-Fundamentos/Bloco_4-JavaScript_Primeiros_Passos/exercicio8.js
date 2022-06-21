1079f4733acca5e5826eefd11ab5c9fb9a52921f
ViniciusRCampos <vinicius_uo@hotmail.com>
2022-06-21 20:16:43 -0300

    Exercicio 4.1 - Verificação da existencia de numero par

---
+++ Modulo_1-Fundamentos/Bloco_4-JavaScript_Primeiros_Passos/exercicio8.js
@@ -0,0 +1,12 @@
+/*🚀 Escreva um programa que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false.
+Bonus: use somente um if.*/
+
+let num1 = 51;
+let num2 = 3;
+let num3 = 1;
+
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0){
+    console.log(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0);
+} else {
+    console.log(num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0);
+}
